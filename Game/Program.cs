4e50a8517d8ee5163c5a6b98c5cc8f1811afbc99
Dmitry Melkov <melkov.dev@yandex.ru>
2026-07-24 01:42:10 +0600

    update engine api

--- Game/Program.cs
+++ Game/Program.cs
@@ -1,25 +1,29 @@
-using Game;
+using Vecxy.Diagnostics;
 using Vecxy.Engine;
+using Vecxy.Kernel;
 
-var assetsPath = Path.Combine(AppContext.BaseDirectory, "Assets");
-#if DEBUG
-assetsPath = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "../../../Assets"));
-#endif
+namespace Game;
 
-
-var options = new EngineOptions
+internal static class Program
 {
-    WindowTitle = "Game",
-    AssetsPath = assetsPath
-};
+    public static void Main(string[] args)
+    {
+        var options = new Engine.Options
+        {
+            Window = new WindowOptions("Game", 800, 600),
+            AssetsPath = Env.IsDev
+                ? Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "../../../Assets"))
+                : Path.Combine(AppContext.BaseDirectory, "Assets")
+        };
 
-var layers = new List<AppLayer>
-{
-    new GameLayer(),
-    new EditorLayer(),
-    new EngineLayer()
-};
+        var layers = new List<AppLayer.IDefinition>
+        {
+            new EngineLayer.Definition(),
+            new GameLayer.Definition(),
+        };
 
-using var engine = new Engine(options, layers);
+        using var engine = new Engine(options, layers);
 
-engine.Run();
+        engine.Run();
+    }
+}
