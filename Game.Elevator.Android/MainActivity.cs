3146645794693393b1c30b37a0e61379ad99e956
Dmitry Melkov <melkov.dev@yandex.ru>
2026-08-01 20:21:41 +0600

    Fix SDL JNI startup registration on Android

--- Game.Elevator.Android/MainActivity.cs
+++ Game.Elevator.Android/MainActivity.cs
@@ -20,6 +20,20 @@ namespace Game.Elevator;
     Theme = "@android:style/Theme.Black.NoTitleBar.Fullscreen")]
 public sealed class MainActivity : SilkActivity
 {
+    public override void LoadLibraries()
+    {
+        base.LoadLibraries();
+    }
+
+    public override void SetOrientationBis(
+        int width,
+        int height,
+        bool resizable,
+        string hint)
+    {
+        base.SetOrientationBis(width, height, resizable, hint);
+    }
+
     protected override void OnRun()
     {
         var filesDirectory = FilesDir?.AbsolutePath ??
@@ -46,6 +60,11 @@ public sealed class MainActivity : SilkActivity
 
         using var engine = new Engine(options, layers);
         engine.Run();
+    }
+
+    protected override void OnDestroy()
+    {
+        base.OnDestroy();
     }
 
 #pragma warning disable CS0672
