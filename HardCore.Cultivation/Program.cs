ddb79e63deac58271540b5dc3624ce498816e368
Dmitry Melkov <melkov.dev@yandex.ru>
2026-08-02 13:40:34 +0600

    refactor(cultivation): use engine platform hosts

--- HardCore.Cultivation/Program.cs
+++ HardCore.Cultivation/Program.cs
@@ -1,33 +1,44 @@
-﻿using Vecxy.Assets;
+using Vecxy.Assets;
+#if !ANDROID
+using Vecxy.Editor;
+#endif
 using Vecxy.Engine;
 using Vecxy.Kernel;
-using HardCore.Cultivation;
-using Vecxy.Editor;
+using Vecxy.Platforms;
 
+namespace HardCore.Cultivation;
+
+public sealed class CultivationApplication : IVecxyApplication
+{
+    public Engine.Options CreateEngineOptions(PlatformContext context) => new()
+    {
+        Headless = false,
+        Window = new IWindow.Options("HardCore Cultivation", 450, 900)
+    };
+
+    public IReadOnlyList<AAppLayer.IDefinition> CreateLayers(PlatformContext context)
+    {
+        var layers = new List<AAppLayer.IDefinition>
+        {
+            new EngineLayer.Definition(new AssetsModule.Options
+            {
+                AssetsDirectory = context.AssetsDirectory
+            })
+        };
+#if !ANDROID
+        layers.Add(new EditorLayer.Definition());
+#endif
+        layers.Add(new GameLayer.Definition());
+        return layers;
+    }
+}
+
+#if !ANDROID
 public static class Program
 {
     public static void Main(string[] args)
     {
-        var options = new Engine.Options
-        {
-            Headless = false,
-            Window = new IWindow.Options("HardCore Cultivation", 450, 900)
-        };
-
-        var assetsOptions = new AssetsModule.Options
-        {
-            AssetsDirectory = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "./Assets"))
-        };
-
-        var layers = new List<AAppLayer.ADefinition>
-        {
-            new EngineLayer.Definition(assetsOptions),
-            new EditorLayer.Definition(),
-            new GameLayer.Definition()
-        };
-
-        using var engine = new Engine(options, layers);
-
-        engine.Run();
+        PlatformRunner.RunDesktop<CultivationApplication>();
     }
 }
+#endif
